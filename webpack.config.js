67c9fc82ea042642485641e90a6048412aab3670
danteYoon <dante@watcha.com>
2021-06-28 20:28:43 +0900

    Update: initial setting

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.txt$/,
+        use: "raw-loader",
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  mode: process.env === "production" ? "production" : "development",
+  optimization: { minimizer: [] },
+}
